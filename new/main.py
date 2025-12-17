13ff30796e01df73fa9414294d4d3c4139a96dc1
yllssnospinning <mrnospinning@gmail.com>
2025-12-17 22:34:43 +0800

    Finished margin manager with function to add order and allocate reducible positions which are freed by removing reduce orders

--- new/main.py
+++ new/main.py
@@ -15,9 +15,10 @@ from OrderBook import OrderBook as OrderBook
 class NashCore:
     def __init__(self, userBalances={}, userOrders={}, userPositions={}, contracts={}):
         # Ensure data integrity:
-        # All user balances are initialised with balance equal to available margin in order to have the margin manager do the work properly;
+        # All user balances are initialised with available margin equal to the balance in order to have the margin manager do the work properly;
         # All userIDs, orderIDs and contractIDs are integers;
 
         self.userBalances = {int(user_id):[balance, balance] for user_id, balance in userBalances.items()}
         self.userOrders = {int(user_id):{int(order_id):order for order_id, order in indv_orders} for user_id, indv_orders in userOrders.items()}
         self.contractInformation = {int(contract_id):contract_information for contract_id, contract_information in contracts.items()}
+        self.userPositions = {int(user_id):{int(contract_id):[position[:], position[:]] for contract_id, position in user_positions.items()} for user_id, user_positions in userPositions.items()}
