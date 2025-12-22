b5c794bf9c033209d95918042e564a498d3c1739
yllssnospinning <mrnospinning@gmail.com>
2025-12-22 16:55:24 +0800

    Somewhat made some steps in the order book

--- docs/CLOBprotocol.md
+++ docs/CLOBprotocol.md
@@ -1,1 +1,6 @@
 # Nash Core Order Matching Protocol
+## Introduction
+The order matching protocol for Nash Core is designed to facilitate both binary- and multi-outcome markets in conditions of low liquidity probably during our initial operations, and prohibit naively exploitable arbitrage conditions from forming. It is engineered to almost be as fast as a single two-sided Limit Order Book while offering the above improvements.  
+A naively exploitable arbitrage condition, in a market where the sum of the resolution value of all associated contracts shall be N, is when the sum of all Best Offers are below N or the sum of all Best Bids are above N, such that a trader may purchase the same quantity of YES or NO contracts for all outcomes and ensure a risk-free profit.
+
+## Operation Principle
--- new/OrderBook.py
+++ new/OrderBook.py
@@ -14,52 +14,58 @@ from orderProperties import OrderProperties as op
 
 class orderBook:
     def __init__(self, _master, contractID):
-        self.fillOrder = _master._fillOrder
-        self.cancelOrder = _master._cancelOrder
+        self.fillOrder = _master.fillOrder
+        self.cancelOrder = _master.cancelOrder
         self.contractID = contractID
+
+        self.linkedBooks = []
 
         # price:[head, tail, qty, number of orders]
         self.book = [SortedDict(key=lambda x:-x), SortedDict(key=lambda x:x)]
         self.bestPrices = [None, None]
 
-    def addOrder(self, order):
+    def fill(self, fill_side, limit_price, order_qty, enforce_best_price):
+        book = self.book[fill_side]
+        prev_bestprice = self.bestPrices[fill_side]
 
+        while True:
+            best_fill_price, best_level = self.bestPrices[fill_side].peekitem(0)
+            # Halt if there are no price levels in this side of the book or t
+            if (not best_fill_price) or (prev_bestprice != best_fill_price): break
+            if (not order_qty) or ((fill_side == 0 and best_fill_price < limit_price) or (fill_side == 1 and best_fill_price > limit_price)): break
+            best_order = best_level[1]
+            fill_qty = min(order_qty, sum(best_order[6]))
+            self.fillOrder
 
-    def fillBestLevel(self, side, lmtPrice, maxQty):
-        best_price = self.bestPrices[side]
-        best_level = self.book[side][best_price]
-
-        if side == 0:
-            if lmtPrice > best_price: return False
-        else:
-            if lmtPrice < best_price: return False
-
-        while best_level[3] and maxQty:
-            best_order = best_level[0]
-            fill_qty = min(maxQty, sum(best_order[6]))
-
-            self.fillOrder(best_order[4], fill_qty)
-            maxQty -= fill_qty
-
-        return maxQty
-
-    # TODO: optimise this function
     def remove_order(self, order):
+        """
+        Removes a specific order from the book.
+        To be used when an order-removing function is called externally of the book.
+        """
         order_price = order[4]
         order_side = order[5]
-        order_head, order_tail = order[7][0], order[7][1]
-        if order_head: order_head[7][1] = order_tail
-        if order_tail: order_head[7][0] = order_head
+
+        # Change the links of the head and tail of the removed order.
+        order_links = order[7]
+        order_links[0][7][1] = order_links[1]
+        order_links[1][7][0] = order_links[0]
+
         side_book = self.book[order_side]
         order_price_level = side_book[order_price]
+
         order_price_level[2] -= sum(order[6])
-        order_price_level[3] - 1
+        order_price_level[3] -= 1
         if not order_price_level[3]:
             del side_book[order_price]
+            # TODO: review syntax vaildity in the context of the sortedcontainers library
             if not side_book:
                 self.bestPrices[order_side] = None
-                return
             self.bestPrices[order_side] = side_book.peekitem(0)[0]
+            return
+
+        # Clever optimisation here: Only attempt to change the head and tail order of the price level if it still exists (i.e. you are not removing the last order from the price level)
+        if order == order_price_level[0]: order_price_level[0] = order_links[1]
+        if order == order_price_level[1]: order_price_level[1] = order_links[0]
 
     def post_order(self, order):
         order[7] = [None, None]
@@ -70,6 +76,7 @@ class orderBook:
             return
         price_level = side_book[order.price]
         order[7][0] = price_level[1]
+        if not price_level[0]: price_level[0] = order
         price_level[1] = order
         price_level[2] += sum(order[6])
         price_level[3] += 1
